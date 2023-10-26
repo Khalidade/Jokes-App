e83363ec9ce5a16773f96bde17859da906ff6733
Khalidade <khalidadeleke24@gmail.com>
2023-10-26 12:29:03 +0100

    initial commit

---
+++ JokesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JokesApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
